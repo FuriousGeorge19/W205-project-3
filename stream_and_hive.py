f7d2a16dd2266d2438ba7fd67f5c166f12f74a26
Joe Mirza <mirza2020@berkeley.edu>
2020-12-03 23:54:12 -0800

    add support for 4 events

--- stream_and_hive.py
+++ stream_and_hive.py
@@ -134,14 +134,14 @@ def main():
 
 
 
-############### start here
 
-    sword_purchases = raw_events \
-        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+
+    default_purchases = raw_events \
+        .filter(is_default(raw_events.value.cast('string'))) \
         .select(raw_events.value.cast('string').alias('raw_event'),
                 raw_events.timestamp.cast('string'),
                 from_json(raw_events.value.cast('string'),
-                          purchase_sword_event_schema()).alias('json')) \
+                          default_event_schema()).alias('json')) \
         .select('raw_event', 'timestamp', 'json.*')
 
 
@@ -158,63 +158,50 @@ def main():
 
 
 
-    sword_purchases = raw_events \
-        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+    armor_purchases = raw_events \
+        .filter(is_armor_purchase(raw_events.value.cast('string'))) \
         .select(raw_events.value.cast('string').alias('raw_event'),
                 raw_events.timestamp.cast('string'),
                 from_json(raw_events.value.cast('string'),
-                          purchase_sword_event_schema()).alias('json')) \
+                          purchase_armor_event_schema()).alias('json')) \
         .select('raw_event', 'timestamp', 'json.*')
 
 
 
 
-    sword_purchases = raw_events \
-        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+    join_guilds = raw_events \
+        .filter(is_join_guild(raw_events.value.cast('string'))) \
         .select(raw_events.value.cast('string').alias('raw_event'),
                 raw_events.timestamp.cast('string'),
                 from_json(raw_events.value.cast('string'),
-                          purchase_sword_event_schema()).alias('json')) \
+                          join_a_guild_event_schema()).alias('json')) \
         .select('raw_event', 'timestamp', 'json.*')
 
 
 
-#############
 
+#default
 
+    spark.sql("drop table if exists default_purchases")
+    sql_string_default = """
+        create external table if not exists default_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/default_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_default)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#sword
 
     spark.sql("drop table if exists sword_purchases")
-    sql_string = """
+    sql_string_sword = """
         create external table if not exists sword_purchases (
             raw_event string,
             timestamp string,
@@ -227,9 +214,66 @@ def main():
             location '/tmp/sword_purchases'
             tblproperties ("parquet.compress"="SNAPPY")
             """
-    spark.sql(sql_string)
+    spark.sql(sql_string_sword)
 
-    sink = sword_purchases \
+
+#armor
+
+    spark.sql("drop table if exists armor_purchases")
+    sql_string_armor = """
+        create external table if not exists armor_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/armor_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_armor)
+
+#guilds
+
+    spark.sql("drop table if exists join_guilds")
+    sql_string_guilds = """
+        create external table if not exists join_guilds (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/join_guilds'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_guilds)
+
+
+
+
+
+
+
+
+
+#default
+
+    sink1 = default_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_default_purchases") \
+        .option("path", "/tmp/default_purchases") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+#sword
+
+    sink2 = sword_purchases \
         .writeStream \
         .format("parquet") \
         .option("checkpointLocation", "/tmp/checkpoints_for_sword_purchases") \
@@ -237,7 +281,29 @@ def main():
         .trigger(processingTime="10 seconds") \
         .start()
 
-    sink.awaitTermination()
+#armor
+
+    sink3 = armor_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_armor_purchases") \
+        .option("path", "/tmp/armor_purchases") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+#guilds
+
+    sink4 = join_guilds \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_join_guilds") \
+        .option("path", "/tmp/join_guilds") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+
+    spark.streams.awaitAnyTermination()
+
 
 
 if __name__ == "__main__":
