d4027b53f898e33a85c0795de1c156ac6f8e981a
Joe Mirza <joe.mirza@gmail.com>
2020-12-06 02:45:27 +0000

    Create temp versions to try to add metadata

---
+++ stream_and_hive_temp.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, from_json
+from pyspark.sql.types import StructType, StructField, StringType
+
+
+def default_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+
+def purchase_sword_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+
+def purchase_armor_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+
+def join_a_guild_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+
+
+@udf('boolean')
+def is_default(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'default':
+        return True
+    return False
+
+
+
+@udf('boolean')
+def is_sword_purchase(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'purchase_sword':
+        return True
+    return False
+
+
+
+@udf('boolean')
+def is_armor_purchase(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'purchase_armor':
+        return True
+    return False
+
+
+
+@udf('boolean')
+def is_join_guild(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'join_a_guild':
+        return True
+    return False
+
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+    raw_events = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .load()
+
+
+
+
+
+    default_purchases = raw_events \
+        .filter(is_default(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          default_event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+
+
+
+
+    sword_purchases = raw_events \
+        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          purchase_sword_event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+
+
+    armor_purchases = raw_events \
+        .filter(is_armor_purchase(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          purchase_armor_event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+
+
+
+    join_guilds = raw_events \
+        .filter(is_join_guild(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          join_a_guild_event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+
+
+
+#default
+
+    spark.sql("drop table if exists default_purchases")
+    sql_string_default = """
+        create external table if not exists default_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/default_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_default)
+
+#sword
+
+    spark.sql("drop table if exists sword_purchases")
+    sql_string_sword = """
+        create external table if not exists sword_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/sword_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_sword)
+
+
+#armor
+
+    spark.sql("drop table if exists armor_purchases")
+    sql_string_armor = """
+        create external table if not exists armor_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/armor_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_armor)
+
+#guilds
+
+    spark.sql("drop table if exists join_guilds")
+    sql_string_guilds = """
+        create external table if not exists join_guilds (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/join_guilds'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sql_string_guilds)
+
+
+
+
+
+
+
+
+
+#default
+
+    sink1 = default_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_default_purchases") \
+        .option("path", "/tmp/default_purchases") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+#sword
+
+    sink2 = sword_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_sword_purchases") \
+        .option("path", "/tmp/sword_purchases") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+#armor
+
+    sink3 = armor_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_armor_purchases") \
+        .option("path", "/tmp/armor_purchases") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+#guilds
+
+    sink4 = join_guilds \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_join_guilds") \
+        .option("path", "/tmp/join_guilds") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+
+    spark.streams.awaitAnyTermination()
+
+
+
+if __name__ == "__main__":
+    main()
